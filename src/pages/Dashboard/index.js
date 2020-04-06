dbfadeda9ec3497020b89677f09be310467a4937
KZTN <kakaiocesar2@gmail.com>
2020-04-06 14:52:59 -0300

    :sparkles: Created file base to extract coronavirus cases to json
    This file requires a bunch on dependencies. Needs python 2x+ and secondary packages: Jupiter, pandas, numpy and camelot

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,36 +1,35 @@
-import React, {useEffect, useState} from 'react';
-import api from '../../services/api';
-import Chart from './chart';
-import './styles.css';
+import React, { useEffect, useState } from "react";
+import api from "../../services/api";
+import Chart from "./chart";
+import "./styles.css";
 export default function Home() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        async function handleAPI() {
-            const response = await api.get('/');
-            setData(response.data);
-        }
-        handleAPI();
-    },[]);
-    return (
-        <div className="dashboard">
-        <h1>Quadro geral</h1>
-        <table>
-            <td>Casos
-                <th>{data.cases}</th>
-            </td>
-            <td>Mortes
-                <th>{data.deaths}</th>
-            </td>
-            <td>Suspeitas
-                <th>{data.suspects}</th>
-            </td>
-            <td>Negados
-            <th>{data.refuses}</th>
-            </td>
-        </table>
-        <Chart/>
-        <p>Data da última atualização: {data.datetime}</p>
-        </div>
-    );
+  useEffect(() => {
+    async function handleAPI() {
+      const response = await api.get("/");
+      setData(response.data);
+    }
+    handleAPI();
+  }, []);
+  return (
+    <div className="dashboard">
+      <table class="rwd-table">
+        <tr>
+          <th>Casos</th>
+          <th>Mortes</th>
+          <th>Suspeitos</th>
+          <th>Descartados</th>
+        </tr>
+        <tr>
+          <td data-th="cases">{data.cases}</td>
+          <td data-th="deaths">{data.deaths}</td>
+          <td data-th="suspects">{data.suspects}</td>
+          <td data-th="refuses">{data.refuses}</td>
+        </tr>
+      </table>
+      <p>Data da última atualização: {data.datetime}</p>
+    </div>
+  );
 }
+
