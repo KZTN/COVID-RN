20eefdbc5bb25740de7d86983e03e85510271b11
KZTN <kakaiocesar2@gmail.com>
2020-04-07 11:44:45 -0300

    :lipstick: Improved footer to responsive screens and format date

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Routes from './routes';
+import emblem from './assets/RN_emblem.png';
 import DateTime from './utils/datetime';
 import './App.css';
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <div className="container">
       <header>
         <div className="title-box">
-          <strong>COVID-19 RN</strong>
+          <h1><strong>COVID-19 RN</strong></h1>
         </div>
       </header>
       <div className="box-situation">
@@ -17,13 +18,27 @@ function App() {
           </div>
       </div>
       <div className="box-datetime">
-        <span>até às 21h do dia 5 de abril</span>
+        <span><DateTime/></span>
       </div>
       <div className="content">
         <main>
           <Routes/>
         </main>
       </div>
+      <footer>
+        <div className="slogan-rn">
+          <span>RIO GRANDE DO NORTE</span>
+          <h2>FORTE NO COMBATE</h2>
+          <h2>AO CORONAVIRUS</h2>
+        </div>
+        <div className="emblema-rn">
+          <img src={emblem} alt="" srcset=""/>
+        </div>
+        <div className="hashtag">
+          <div className="hashtag-item"><span>#FIQUEEMCASA</span></div>
+          <div className="hashtag-item"><span>#RNCONTRACOVID-19</span></div>
+        </div>
+      </footer>
     </div>
   );
 }
--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import * as moment from 'moment';
+import 'moment/locale/pt-br';
 import api from '../services/api'
 export default function DateTime() {
   const [data, setData] = useState([]);
@@ -6,13 +8,15 @@ export default function DateTime() {
   useEffect(() => {
     async function handleAPI() {
       const response = await api.get("/");
-      setData(response.data);
+      const formatedDate = moment(response.data.datetime).utcOffset("-03:00").format("HH DD MMMM", 'pt-BR');
+      const formatedDateArr = formatedDate.split(' ');
+      setData(formatedDateArr);
     }
     handleAPI();
   }, []);
   return (
       <>
-        <span>{data.datetime}</span>
+        Até às {data[0]}h do dia {data[1]} de {data[2]}
       </>
   );
 }
