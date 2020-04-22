b4f4c27400f2570db2e39a8da11a66037ac13872
KZTN <kakaiocesar2@gmail.com>
2020-04-21 21:20:50 -0300

    :lipstick: Updated footer styles
    all changes are needs to be revised and tested

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {FaEnvelope} from 'react-icons/fa'
+import {FaEnvelope, FaGlobe} from 'react-icons/fa'
 import icon from '../../assets/icon.png'
 import Emoji from '../../interfaces/emoji'
 import './styles.css'
@@ -7,20 +7,36 @@ export default function Footer() {
 
     return (
         <footer>
-    <div className="item" style={{marginTop: 20}}>
-        <div className="box-madewith">
-          <a href="https://github.com/KZTN" style={{color: "inherit"}}> Feito com <Emoji symbol="💙"/></a>
-        </div> 
+          <div className="item">
+            <div className="box-column">
+              <div className="box-reference">
+                <div className="box-madewith">
+                  <a href="https://github.com/KZTN" style={{color: "inherit", fontWeight: "bold", fontSize: 15}}> Feito com <Emoji symbol="💙"/></a>
+                </div> 
+              </div>
+              <div className="box-reference">
+                <div className="reference-msg"><a href="http://www.saude.rn.gov.br/" target="_blank" rel="noopener noreferrer" style={{color: "inherit"}}>Fonte de dados: SESAP</a></div>
+                <div className="reference-icon"><a href="http://www.saude.rn.gov.br/" target="_blank" rel="noopener noreferrer" style={{color: "inherit"}}><FaGlobe size={14} color="#353244"/></a></div>
+              </div>
+              <div className="box-reference">
+                <div className="reference-msg"><a href="mailto: oicovidrn@gmail.com" style={{color: "inherit"}}>Contato: EMAIL</a></div>
+                <div className="reference-icon"><a href="mailto: oicovidrn@gmail.com" style={{color: "inherit"}}><FaEnvelope size={14} color="#353244"/></a></div>
+              </div>
+            </div>
+          </div>
+    <div className="item">
         <hr/>
-    <div className="box-hashtag" style={{marginTop: 10}}>
+    <div className="box-hashtag">
         <div className="hashtag-item">#FIQUEEMCASA</div>
         <div className="hashtag-item">#RNNAQUARENTENA</div>
     </div>
     </div>
-    <div className="item box-column">
-      <img src={icon} alt="logomarca COVID-RN" style={{width: 64, height: 64}}/>
-      <span>© 2020 COVID-RN</span>
-      <span>Release v0.7156</span>
+    <div className="item">
+      <div className="box-column">
+        <img src={icon} alt="logomarca COVID-RN" style={{width: 64, height: 64}}/>
+        <span>© 2020 COVID-RN</span>
+        <span>Release v0.7162</span>
+      </div>
     </div>
   </footer>
     );
