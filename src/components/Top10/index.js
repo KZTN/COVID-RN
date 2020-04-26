8aa9548696d564007f9d95b71af1c6c172a335bd
kztn <kakaiocesar2@gmail.com>
2020-04-25 23:51:23 -0300

    ⚡️ Improved slick performance on desktop screens
    all changes are functional

--- src/components/Top10/index.js
+++ src/components/Top10/index.js
@@ -48,7 +48,7 @@ useEffect(() => {
 const Wrapper = styled.div`
 width: 100%;
 margin: 30px auto 30px auto;
-display: flex,
+display: flex;
 flex-direction: column;
     
 `;
@@ -76,7 +76,7 @@ align-items: center;
                     slidesToScroll={isMobile?1:3}
                     dots={true}
                     adaptiveHeight={true}
-                    arrows={false}
+                    arrows={isMobile?false:true}
                 >
                     <Page><Content className={isMobile?"top-content-mobile":"top-content"}><strong>{boxcountcities}</strong><span>Cidades sob monitoramento</span></Content></Page>
                     <Page><Content className={isMobile?"top-content-mobile":"top-content"}><strong>{boxaffectedcities}</strong><span>Cidades afetadas</span></Content></Page>
